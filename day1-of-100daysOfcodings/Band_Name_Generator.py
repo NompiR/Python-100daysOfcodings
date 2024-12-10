aff3ba17eb1cf439cb3b9bbc099b00374d4c7185
Nompi R <nompir.1986@gmail.com>
2024-12-11 03:27:11 +0530

    Create Band_Name_Generator.py

---
+++ day1-of-100daysOfcodings/Band_Name_Generator.py
@@ -0,0 +1,15 @@
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be:  " + city + " " + pet )
+
+"""
+OUTPUT:-
+
+Welcome to the Band Name Generator.
+Which city did you grow up in?
+Bengaluru
+What is the name of a pet?
+Rani
+Your band name could be: Bengaluru Rani
+"""
