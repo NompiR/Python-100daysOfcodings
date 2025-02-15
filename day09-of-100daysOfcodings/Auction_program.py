ea220fa53f7d6f15878abe4772a0088a823bc465
Nompi R <nompir.1986@gmail.com>
2025-02-15 11:15:19 +0530

    Update Auction_program.py

--- day09-of-100daysOfcodings/Auction_program.py
+++ day09-of-100daysOfcodings/Auction_program.py
@@ -4,7 +4,7 @@ def find_highest_bidder(bidding_record):
     highest = 0
     winner = ''
     for bidder in bidding_record:
-        bid_amount = bidding[bidder]
+        bid_amount = bidding_record[bidder]
         if bid_amount > highest:
             highest = bid_amount
             winner = bidder
